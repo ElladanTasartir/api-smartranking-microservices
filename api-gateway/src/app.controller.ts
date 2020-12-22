ea648b1994c95de2e090822de5efbe0192cd8bf7
Erick Malta <erickmalta100@gmail.com>
2020-12-21 23:47:21 -0300

    feat(app): add ClientProxy and create-category

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Body,
+  Controller,
+  Logger,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
+import { CreateCategoryDTO } from './dtos/create-category.dto';
 
-@Controller()
+@Controller('api/v1')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private logger = new Logger(AppController.name);
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  private clientAdminBackend: ClientProxy;
+
+  constructor() {
+    this.clientAdminBackend = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://localhost:5672/smartranking'],
+        queue: 'admin-backend',
+      },
+    });
+  }
+
+  @Post('categories')
+  @UsePipes(ValidationPipe)
+  async createCategory(@Body() createCategoryDTO: CreateCategoryDTO) {
+    return this.clientAdminBackend.emit('create-category', createCategoryDTO);
   }
 }
