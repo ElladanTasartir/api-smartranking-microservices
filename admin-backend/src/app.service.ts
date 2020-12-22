d55e3ef79eb522cfafb6d26c87d0690d0e2deea4
Erick Malta <erickmalta100@gmail.com>
2020-12-22 01:21:31 -0300

    feat(app): implement database logic

---
+++ admin-backend/src/app.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Category } from './interfaces/categories/category.interface';
+import { Player } from './interfaces/players/player.interface';
+
+@Injectable()
+export class AppService {
+  private readonly logger = new Logger(AppService.name);
+
+  constructor(
+    @InjectModel('Category')
+    private readonly categoryModel: Model<Category>,
+    @InjectModel('Player')
+    private readonly playerModel: Model<Player>,
+  ) {}
+
+  async getCategories(): Promise<Category[]> {
+    return this.categoryModel.find();
+  }
+
+  async getCategory(_id: string): Promise<Category> {
+    const category = await this.categoryModel.findOne({ _id });
+
+    if (!category) {
+      throw new RpcException(`Category with ID "${_id}" not found`);
+    }
+
+    return category;
+  }
+
+  async createCategory(category: Category): Promise<Category> {
+    try {
+      const createdCategory = new this.categoryModel(category);
+      return createdCategory.save();
+    } catch (err) {
+      this.logger.error(`Error: ${JSON.stringify(err.message)}`);
+      throw new RpcException(err.message);
+    }
+  }
+}
