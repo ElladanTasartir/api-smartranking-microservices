dfe061da7b43a2e0da08f6fc4fa7faeb1dc01544
Erick Malta <erickmalta100@gmail.com>
2020-12-22 22:34:10 -0300

    feat(interceptors): add interceptros

---
+++ api-gateway/src/interceptors/logging.interceptor.ts
@@ -0,0 +1,19 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const now = Date.now();
+
+    return next
+      .handle()
+      .pipe(tap(() => console.log(`Request duration: ${Date.now() - now}ms`)));
+  }
+}
