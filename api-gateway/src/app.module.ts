f560ec8f69e14394bd693faa6af812a2728bd5df
Erick Malta <erickmalta100@gmail.com>
2020-12-29 22:06:11 -0300

    feat(api-gateway): import players module

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CategoriesModule } from './categories/categories.module';
+import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [CategoriesModule],
+  imports: [CategoriesModule, PlayersModule],
 })
 export class AppModule {}
