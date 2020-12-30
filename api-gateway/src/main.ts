a295f762f4acfc4d192e0a1d92b2d9590079515e
Erick Malta <erickmalta100@gmail.com>
2020-12-29 22:05:08 -0300

    fix(api-gateway): fixed imports

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as momentTimezone from 'moment-timezone';
-import { AllExceptionsFilter } from './filters/http-exception.filter';
-import { LoggingInterceptor } from './interceptors/logging.interceptor';
-import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
+import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import { LoggingInterceptor } from './common/filters/interceptors/logging.interceptor';
+import { TimeoutInterceptor } from './common/filters/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
