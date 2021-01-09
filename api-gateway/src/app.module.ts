2f34b1d1862b2592f186cd44194605b6143d81a2
Erick Malta <erickmalta100@gmail.com>
2021-01-08 22:53:44 -0300

    feat(app): add ConfigModule

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule } from '@nestjs/config';
 import { join } from 'path';
 import { CategoriesModule } from './categories/categories.module';
 import { PlayersModule } from './players/players.module';
@@ -9,6 +10,9 @@ import { UploadsModule } from './uploads/uploads.module';
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'tmp', 'images'),
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
     CategoriesModule,
     PlayersModule,
