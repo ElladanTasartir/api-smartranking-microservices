a47fa74483ce9bd6c6815a18174ef928ffa1888b
Erick Malta <erickmalta100@gmail.com>
2021-01-08 22:57:57 -0300

    feat(modules): add ClientProxy as provider

--- api-gateway/src/categories/categories.module.ts
+++ api-gateway/src/categories/categories.module.ts
@@ -1,20 +1,29 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+import {
+  ClientProxyFactory,
+  RmqOptions,
+  Transport,
+} from '@nestjs/microservices';
 import { CategoriesController } from './categories.controller';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'admin-backend',
-        transport: Transport.RMQ,
-        options: {
-          urls: ['amqp://localhost:5672/smartranking'],
-          queue: 'admin-backend',
-        },
-      },
-    ]),
-  ],
   controllers: [CategoriesController],
+  providers: [
+    {
+      provide: 'admin-backend',
+      useFactory: (configService: ConfigService) => {
+        const clientConfig: RmqOptions = {
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RABBITMQ_HOST')],
+            queue: configService.get<string>('RABBITMQ_QUEUE'),
+          },
+        };
+        return ClientProxyFactory.create(clientConfig);
+      },
+      inject: [ConfigService],
+    },
+  ],
 })
 export class CategoriesModule {}
--- api-gateway/src/players/players.module.ts
+++ api-gateway/src/players/players.module.ts
@@ -1,22 +1,31 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+import {
+  ClientProxyFactory,
+  RmqOptions,
+  Transport,
+} from '@nestjs/microservices';
 import { UploadsModule } from '../uploads/uploads.module';
 import { PlayersController } from './players.controller';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'admin-backend',
-        transport: Transport.RMQ,
-        options: {
-          urls: ['amqp://localhost:5672/smartranking'],
-          queue: 'admin-backend',
-        },
-      },
-    ]),
-    UploadsModule,
-  ],
+  imports: [UploadsModule],
   controllers: [PlayersController],
+  providers: [
+    {
+      provide: 'admin-backend',
+      useFactory: (configService: ConfigService) => {
+        const clientConfig: RmqOptions = {
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RABBITMQ_HOST')],
+            queue: configService.get<string>('RABBITMQ_QUEUE'),
+          },
+        };
+        return ClientProxyFactory.create(clientConfig);
+      },
+      inject: [ConfigService],
+    },
+  ],
 })
 export class PlayersModule {}
