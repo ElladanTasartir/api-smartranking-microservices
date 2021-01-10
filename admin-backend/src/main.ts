d5c73eaaa42868d6fb38571b68ea1d0ce5f3cd58
Erick Malta <erickmalta100@gmail.com>
2021-01-09 22:37:06 -0300

    feat(main): add configService env vars

--- admin-backend/src/main.ts
+++ admin-backend/src/main.ts
@@ -1,16 +1,18 @@
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 const logger = new Logger('Main');
+const configService = new ConfigService();
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.RMQ,
     options: {
-      urls: ['amqp://localhost:5672/smartranking'],
-      queue: 'admin-backend',
+      urls: [configService.get('RABBITMQ_HOST')],
+      queue: configService.get('RABBITMQ_QUEUE'),
       noAck: false,
     },
   });
