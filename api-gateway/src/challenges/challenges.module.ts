e27d72545f7373dba6ca068dd71a3b5bc7f7c4f1
Erick Malta <erickmalta100@gmail.com>
2021-01-11 00:31:42 -0300

    feat(challenges): add ChallengesModule ClientProxy

--- api-gateway/src/challenges/challenges.module.ts
+++ api-gateway/src/challenges/challenges.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import {
+  ClientProxyFactory,
+  RmqOptions,
+  Transport,
+} from '@nestjs/microservices';
 
-@Module({})
+@Module({
+  providers: [
+    {
+      provide: 'challenges-service',
+      useFactory: (configService: ConfigService) => {
+        const clientConfig: RmqOptions = {
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RABBITMQ_HOST')],
+            queue: configService.get<string>('RABBITMQ_QUEUE_CHALLENGES'),
+          },
+        };
+        return ClientProxyFactory.create(clientConfig);
+      },
+      inject: [ConfigService],
+    },
+  ],
+})
 export class ChallengesModule {}
