83b44e7387732435f2efff05882f68485978e38b
Erick Malta <erickmalta100@gmail.com>
2021-01-11 23:13:58 -0300

    refactor(players): change category to be of type Category

--- api-gateway/src/players/interfaces/player.interface.ts
+++ api-gateway/src/players/interfaces/player.interface.ts
@@ -1,8 +1,10 @@
+import { Category } from 'src/categories/interfaces/category.interface';
+
 export interface Player {
   readonly _id: string;
   readonly phoneNumber: string;
   readonly email: string;
-  category: string;
+  category: Category;
   name: string;
   ranking: string;
   position: number;
