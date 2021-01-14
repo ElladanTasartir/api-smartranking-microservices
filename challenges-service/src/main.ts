8b4e033fcc2e4a82d0b71cab54e772708959cf90
Erick Malta <erickmalta100@gmail.com>
2021-01-13 22:03:34 -0300

    feat(main): add messagebroker connection

--- challenges-service/src/main.ts
+++ challenges-service/src/main.ts
@@ -1,8 +1,23 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
+const logger = new Logger('Main');
+
+const configService = new ConfigService();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [configService.get('RABBITMQ_HOST')],
+      queue: configService.get('RABBITMQ_QUEUE'),
+      noAck: false,
+    },
+  });
+
+  app.listen(() => logger.log('Microservice is listening'));
 }
 bootstrap();
