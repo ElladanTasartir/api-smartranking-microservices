c9dc5601657d1f29a5d36624862e603cbab32cdf
Erick Malta <erickmalta100@gmail.com>
2021-01-13 22:04:24 -0300

    feat(app): add mongoose connection and usage of configmodule

--- challenges-service/src/app.module.ts
+++ challenges-service/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ChallengesModule } from './challenges/challenges.module';
+import { MatchesModule } from './matches/matches.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true,
+      }),
+    }),
+    ChallengesModule,
+    MatchesModule,
+  ],
 })
 export class AppModule {}
