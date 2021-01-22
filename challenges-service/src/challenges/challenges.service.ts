e69c19476f83e06498ce3e28d7d76d5982030199
Erick Malta <erickmalta100@gmail.com>
2021-01-22 20:10:26 -0300

    feat(challenges-service): implement get-challenge

--- challenges-service/src/challenges/challenges.service.ts
+++ challenges-service/src/challenges/challenges.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Challenge } from './interfaces/challenge.interface';
@@ -12,6 +13,16 @@ export class ChallengesService {
 
   async findChallengesByPlayerId(_id: string): Promise<Challenge[]> {
     return this.challengesModel.find().where('players').in([_id]);
+  }
+
+  async findChallengeById(_id: string): Promise<Challenge> {
+    const challenge = await this.challengesModel.findOne({ _id });
+
+    if (!challenge) {
+      throw new RpcException(`Challenge with the ID "${_id}" doesn't exist`);
+    }
+
+    return challenge;
   }
 
   async createChallenge(challenge: Challenge): Promise<Challenge> {
