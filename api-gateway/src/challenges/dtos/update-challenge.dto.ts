195d2ca22e76257cae94c69d331a7dd8adc9f8d5
Erick Malta <erickmalta100@gmail.com>
2021-01-25 04:01:53 -0300

    feat(dto): add date field

--- api-gateway/src/challenges/dtos/update-challenge.dto.ts
+++ api-gateway/src/challenges/dtos/update-challenge.dto.ts
@@ -1,8 +1,12 @@
-import { IsOptional, IsString } from 'class-validator';
+import { IsDateString, IsNotEmpty, IsString } from 'class-validator';
 import { ChallengeStatus } from '../enums/challenge-status.enum';
 
 export class UpdateChallengeDTO {
-  @IsOptional()
+  @IsNotEmpty()
+  @IsDateString()
+  date: Date;
+
+  @IsNotEmpty()
   @IsString()
   status: ChallengeStatus;
 }
