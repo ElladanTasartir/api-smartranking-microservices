c5b168d146c205ae7b4a56279f7c9ee08c523703
Erick Malta <erickmalta100@gmail.com>
2021-03-01 00:53:21 -0300

    feat(challenges): add rankings-service rmq client

--- challenges-service/src/challenges/challenges.module.ts
+++ challenges-service/src/challenges/challenges.module.ts
@@ -4,6 +4,8 @@ import { ChallengeSchema } from './interfaces/challenge.schema';
 import { ChallengesController } from './challenges.controller';
 import { ChallengesService } from './challenges.service';
 import { MatchesModule } from 'src/matches/matches.module';
+import { ClientProxyFactory, RmqOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -16,6 +18,19 @@ import { MatchesModule } from 'src/matches/matches.module';
     MatchesModule,
   ],
   controllers: [ChallengesController],
-  providers: [ChallengesService],
+  providers: [ChallengesService, {
+    provide: 'rankings-service',
+    useFactory: (configService: ConfigService) => {
+      const clientConfig: RmqOptions = {
+        transport: Transport.RMQ,
+        options: {
+          urls: [configService.get<string>('RABBITMQ_HOST')],
+          queue: configService.get<string>('RABBITMQ_QUEUE_RANKINGS'),
+        },
+      };
+      return ClientProxyFactory.create(clientConfig);
+    },
+    inject: [ConfigService],
+  },],
 })
 export class ChallengesModule {}
